b818af9addb96657c8b1671801ef4218df8b8d2b
lewmoore <lewis.moore2707@gmail.com>
2018-08-09 10:28:19 +0100

    Alter getXml to get Purchase Order from POR source file

--- Program.cs
+++ Program.cs
@@ -10,21 +10,26 @@ namespace ReportingXML
     {
         static void Main(string[] args){
             var xml = new Program();
-            Console.WriteLine(xml.getXml());
+            Console.WriteLine(xml.getPurchaseOrder());
         }
 
-        public object getXml(){
+        public object getPurchaseOrder(){
            XmlDocument doc = new XmlDocument();
             doc.Load("/Users/moorel/Desktop/Projects/C#/O2/DummyFiles/SupplyChainSourceFiles/POR/POR_SALES_8307_20180201164154.xml");
             XmlNode root = doc.DocumentElement;
 
             XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);  
-            nsmgr.AddNamespace("sshd", "urn:rosettanet:specification:system:StandardDocumentHeader:xsd:schema:01.23"); 
+            nsmgr.AddNamespace("udc", "urn:rosettanet:specification:universal:Document:xsd:schema:01.12"); 
+            nsmgr.AddNamespace("udct", "urn:rosettanet:specification:universal:DocumentType:xsd:codelist:01.13");
+            nsmgr.AddNamespace("tns", "urn:rosettanet:specification:interchange:PurchaseOrderRequest:xsd:schema:02.05");
 
             XmlNode node = root.SelectSingleNode(
-                "descendant::sshd:DocumentHeader/sshd:DocumentInformation/sshd:Creation", nsmgr);
-                
-            return node.InnerXml;
+                "descendant::tns:PurchaseOrder/udc:BusinessDocumentReference[udct:DocumentType='SAO']/udc:Identifier", nsmgr);
+            if (node != null) {
+                return node.InnerXml;
+            } else {
+                return "Error in Node";
+            }
         }
     }
 }
