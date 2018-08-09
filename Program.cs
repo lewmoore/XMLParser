3ed06c55b4a4962409a16d4223ba856bf869a1eb
lewmoore <lewis.moore2707@gmail.com>
2018-08-09 11:21:12 +0100

    Add namespace strings as class variables

--- Program.cs
+++ Program.cs
@@ -8,10 +8,17 @@ namespace ReportingXML
 {
     class Program
     {
+
+        private string udc = "urn:rosettanet:specification:universal:Document:xsd:schema:01.12";
+        private string udct = "urn:rosettanet:specification:universal:DocumentType:xsd:codelist:01.13";
+        private string tns = "urn:rosettanet:specification:interchange:PurchaseOrderRequest:xsd:schema:02.05";
+        private string dp = "urn:rosettanet:specification:domain:Procurement:xsd:schema:02.29";
+
         static void Main(string[] args){
             var xml = new Program();
             Console.WriteLine(xml.getPurchaseOrder());
             Console.WriteLine(xml.getChildOrderID());
+            Console.WriteLine(xml.getOrderVolume());
         }
 
         public object getPurchaseOrder(){
@@ -20,9 +27,9 @@ namespace ReportingXML
             XmlNode root = doc.DocumentElement;
 
             XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);  
-            nsmgr.AddNamespace("udc", "urn:rosettanet:specification:universal:Document:xsd:schema:01.12"); 
-            nsmgr.AddNamespace("udct", "urn:rosettanet:specification:universal:DocumentType:xsd:codelist:01.13");
-            nsmgr.AddNamespace("tns", "urn:rosettanet:specification:interchange:PurchaseOrderRequest:xsd:schema:02.05");
+            nsmgr.AddNamespace("udc", udc); 
+            nsmgr.AddNamespace("udct", udct);
+            nsmgr.AddNamespace("tns", tns);
 
             XmlNode node = root.SelectSingleNode(
                 "descendant::tns:PurchaseOrder/udc:BusinessDocumentReference[udct:DocumentType='SAO']/udc:Identifier", nsmgr);
@@ -39,12 +46,30 @@ namespace ReportingXML
             XmlNode root = doc.DocumentElement;
 
             XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);  
-            nsmgr.AddNamespace("udc", "urn:rosettanet:specification:universal:Document:xsd:schema:01.12"); 
-            nsmgr.AddNamespace("udct", "urn:rosettanet:specification:universal:DocumentType:xsd:codelist:01.13");
-            nsmgr.AddNamespace("tns", "urn:rosettanet:specification:interchange:PurchaseOrderRequest:xsd:schema:02.05");
+            nsmgr.AddNamespace("udc", udc); 
+            nsmgr.AddNamespace("udct", udct);
+            nsmgr.AddNamespace("tns", tns);
 
             XmlNode node = root.SelectSingleNode(
                 "descendant::tns:PurchaseOrder/udc:BusinessDocumentReference[udct:DocumentType='DOR']/udc:Identifier", nsmgr);
+            if (node != null) {
+                return node.InnerXml;
+            } else {
+                return "Error in Node";
+            }
+        }
+
+        public object getOrderVolume(){
+            XmlDocument doc = new XmlDocument();
+            doc.Load("/Users/moorel/Desktop/Projects/C#/O2/DummyFiles/SupplyChainSourceFiles/POR/POR_SALES_8307_20180201164154.xml");
+            XmlNode root = doc.DocumentElement;
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);  
+            nsmgr.AddNamespace("tns", tns);
+            nsmgr.AddNamespace("dp", dp);
+
+            XmlNode node = root.SelectSingleNode(
+                "descendant::tns:PurchaseOrder/tns:OrderLineItem/tns:ProductLineItem/dp:OrderQuantity/dp:RequestedQuantity", nsmgr);
             if (node != null) {
                 return node.InnerXml;
             } else {
