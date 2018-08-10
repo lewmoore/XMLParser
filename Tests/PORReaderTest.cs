539936c54f394d7a229770880ebfc729816f2101
lewmoore <lewis.moore2707@gmail.com>
2018-08-10 12:02:54 +0100

    Add test folder. Setup instantiation of PORReader object

--- Tests/PORReaderTest.cs
+++ Tests/PORReaderTest.cs
@@ -4,12 +4,11 @@ namespace Telefonica {
 
     [TestFixture]
     public class PORReaderTest {
+    PORReader reader = new PORReader("/Users/moorel/Desktop/Projects/C#/O2/DummyFiles/SupplyChainSourceFiles/POR/POR_SALES_8307_20180201164154.xml");
+
         [Test]
         public void getPurchaseOrder(){
-
-            PORReader reader = new PORReader("/Users/moorel/Desktop/Projects/C#/O2/DummyFiles/SupplyChainSourceFiles/POR/POR_SALES_8307_20180201164154.xml");
             Assert.AreEqual(reader.getPurchaseOrder(), "aa036E31tc63qjPfIMJ");
-            // Assert.IsNotNull(reader.root);
         }
     }
 }
